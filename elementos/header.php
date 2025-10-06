a1fb7fc00d9ba4c14d969c1be63fcab3fe57a7ce
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-10-06 10:23:58 +0200

    Título cambiado a 'Bienvenido a Camas'

--- elementos/header.php
+++ elementos/header.php
@@ -3,7 +3,7 @@
 </head>
 
 <header class="text-center bg-white p-3 rounded shadow-sm">
-  <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
+  <h1 class="text-primary">Bienvenido a Camas</h1>
   <p class="text-muted">Usando include() por primera vez</p>
   
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
