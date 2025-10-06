0a281e7aadf7285949f1095bf27c86d3ca56916c
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-10-06 10:22:32 +0200

    Título cambiado a 'Mi Sitio Dinámico PHP'

--- elementos/header.php
+++ elementos/header.php
@@ -3,7 +3,7 @@
 </head>
 
 <header class="text-center bg-white p-3 rounded shadow-sm">
-  <h1 class="text-primary">🌟 Bienvenido a mi primer sitio PHP 🌟</h1>
+  <h1 class="text-primary">Mi Sitio Dinámico PHP</h1>
   <p class="text-muted">Usando include() por primera vez</p>
   
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
